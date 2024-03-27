3d0c5df3f01ed61779cb739ee11677846cfbedbf
parbhatsingh5211 <parbhatsingh.sr@gmail.com>
2024-03-27 22:43:05 +0530

    add some functionality

--- src/components/Tours.js
+++ src/components/Tours.js
@@ -1,7 +1,19 @@
-import React from 'react'
-
-export const Tours = () => {
-  return (
-    <div>Tours</div>
-  )
+import Card from './Card'
+function Tours({tours, removeTour}) {
+    return (
+        <div className='container'>
+            <div>
+                <h2 className='title'> Plan With Love</h2>
+            </div>
+            <div className='cards'>
+                {
+                    tours.map( (tour) => {
+                        return <Card key={tour.id} {...tour} removeTour={removeTour}></Card>
+                    } )
+                }
+            </div>
+        </div>
+    );
 }
+
+export default Tours;
